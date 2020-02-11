f7b448c344c8ab4358e6ab60e9e25d907a143a7e
Alexandra Travkina <hello@fixel.ru>
2020-02-11 21:40:20 +0700

    Upload

--- work/resources/js/template.js
+++ work/resources/js/template.js
@@ -232,3 +232,11 @@ $(document).ready(function() {
         }
     });  
 });
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).html()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
